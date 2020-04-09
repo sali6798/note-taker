5e063f37ed28764c03dd75325def62e5e7cb798a
sali6798 <shaidee@alingcastre.net>
2020-04-08 20:35:38 -0700

    added html request for / route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,17 @@ const router = express.Router();
 const path = require("path");
 
 //================ HTML GET Requests ===============
+// displays index.html
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 // displays notes.html when user visits notes page
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-// displays index.html if user tries to visit any other route
+// defaults to index.html if user tries to visit any other route
 router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
